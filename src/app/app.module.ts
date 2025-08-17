97a781677c78dc5bf214474b58c7392a47ed43af
Tuong Hoang <httuong.2705@gmail.com>
2025-08-17 11:03:03 +0300

    SW-8: fix and add some missng module to task module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
@@ -12,7 +11,7 @@ import { SharedModule } from "./shared/shared.module";
 @NgModule({
     declarations: [AppComponent, HeaderComponent, UserComponent],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule, SharedModule, TasksModule], // BrowserModule already includes DatePipe
+    imports: [BrowserModule, SharedModule, TasksModule], // BrowserModule already includes DatePipe
 })
 export class AppModule {
 
